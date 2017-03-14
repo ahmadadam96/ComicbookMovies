2cf7fa189442ed7acdf8e94e4f85dbce4c33a997
ahmad <ahmadadam96@gmail.com>
2017-03-14 10:00:36 +0300

    Initial commit

---
+++ app/src/main/java/ahmadadam96/comicbookmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package ahmadadam96.comicbookmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
