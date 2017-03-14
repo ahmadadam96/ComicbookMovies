3916446a300889592429465dcfdfc316ae897c75
ahmad <ahmadadam96@gmail.com>
2017-03-14 19:50:31 +0300

    Got the poster to display Added the overview of the movie to the right of the poster and bottom of the title

--- app/src/main/java/ahmadadam96/comicbookmovies/MovieAdapter.java
+++ app/src/main/java/ahmadadam96/comicbookmovies/MovieAdapter.java
@@ -1,12 +1,18 @@
 package ahmadadam96.comicbookmovies;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -67,6 +73,34 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         //Set the text of the TextView to be of the daysleft
         daysView.setText(diffInDays.toString());
 
+        new DownloadImageTask((ImageView) listItemView.findViewById(R.id.poster))
+                .execute("https://image.tmdb.org/t/p/w500/" + currentMovie.getPosterUrl());
+
+        TextView overview = (TextView) listItemView.findViewById(R.id.overview);
+        overview.setText(currentMovie.getOverview());
+
         return listItemView;
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
