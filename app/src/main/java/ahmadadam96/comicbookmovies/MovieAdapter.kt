7609d967cb6d12c57b34abed13776ac3c2810631
ahmadadam96 <ahmadadam96@gmail.com>
2017-05-21 00:38:34 -0300

    Converted MovieAdapter and MovieCode classes to kotlin

---
+++ app/src/main/java/ahmadadam96/comicbookmovies/MovieAdapter.kt
@@ -0,0 +1,44 @@
+package ahmadadam96.comicbookmovies
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import java.util.ArrayList
+
+/**
+ * Created by ahmad on 2017-03-14.
+ */
+
+class MovieAdapter(private val context: Context, private val itemResource: Int, private var movies: ArrayList<Movie>?) : RecyclerView.Adapter<MovieHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(this.itemResource, parent, false)
+        return MovieHolder(this.context, view)
+    }
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        val movie = this.movies!![position]
+        holder.bindMovie(movie)
+
+    }
+
+    override fun getItemCount(): Int {
+        return this.movies!!.size
+    }
+
+    fun clear() {
+        if (movies!!.size > 0) {
+            movies!!.clear()
+            notifyDataSetChanged()
+        }
+    }
+
+    fun update(movieList: ArrayList<Movie>) {
+        movies = movieList
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/ahmadadam96/comicbookmovies/MovieCode.kt
@@ -0,0 +1,7 @@
+package ahmadadam96.comicbookmovies
+
+/**
+ * Created by ahmad on 2017-03-19.
+ */
+
+class MovieCode internal constructor(val code: String, val universe: String)
