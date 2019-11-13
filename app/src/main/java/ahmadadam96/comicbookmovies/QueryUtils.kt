871d1eea7078ba2e94566cb1fd4411878aa96f67
Ahmed El Yaacoub <ahmadadam96@gmail.com>
2019-11-13 21:20:21 +0100

    Updated libraries

--- app/src/main/java/ahmadadam96/comicbookmovies/QueryUtils.kt
+++ app/src/main/java/ahmadadam96/comicbookmovies/QueryUtils.kt
@@ -18,14 +18,12 @@ package ahmadadam96.comicbookmovies
 import android.content.Context
 import android.text.TextUtils
 import android.util.Log
-import android.widget.Toast
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.IllegalStateException
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
